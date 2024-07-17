2a81133ed8a14d9d9b9a391d9d752bffc053cbce
Safmica <safmikasa@gmail.com>
2024-07-17 16:34:52 +0700

    fix (routes/user): user logout remove middleware

--- routes/User.go
+++ routes/User.go
@@ -11,7 +11,7 @@ func UserRoutes(app *fiber.App) {
 	app.Get("/users", middleware.Auth, controller.GetAllUser)
 	app.Get("/users/:id", middleware.Auth, controller.GetUserByID)
 	app.Post("/users/login", controller.UserLogin)
-	app.Post("users/logout", middleware.Auth, controller.UserLogout)
+	app.Post("users/logout", controller.UserLogout)
 	app.Post("/users", middleware.Auth, controller.CreateUser)
 	app.Put("/users/:id", middleware.Auth, controller.UpdateUser)
 	app.Delete("/users/:id", middleware.Auth, controller.DeleteUser)
